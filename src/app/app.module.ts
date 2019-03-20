08e8560d840d710de9be24fde3e85a65d4d27b16
Daniel Weiß <Daniel.Weiss@grollmus.de>
2019-03-20 14:01:27 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HighscoreComponent } from './highscore/highscore.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HighscoreComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
