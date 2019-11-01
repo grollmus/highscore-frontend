dd01c58bfd0d1d73bcab9f676c20086a2df28d4f
Daniel Weiß <wanieldeiss@bonestorm-mbp.fritz.box>
2019-11-01 11:17:18 +0100

    add dummy login form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as dialogPolyfill from 'dialog-polyfill/dist/dialog-polyfill';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  loginDialog: HTMLDialogElement;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.loginDialog = document.querySelector('#dialog-login');
+    dialogPolyfill.registerDialog(this.loginDialog);
   }
-
 }
