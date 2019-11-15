f5d3f3f64536b90aad4efd6adcef2def6e8a3779
Daniel Weiß <wanieldeiss@gmail.com>
2019-11-15 07:49:21 +0100

    init delete player

--- src/app/modules/admin/pages/all-players/all-players.component.ts
+++ src/app/modules/admin/pages/all-players/all-players.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Player } from '@app/models';
 import { PlayerService } from '@app/services';
+import {
+  FormGroup,
+  FormControlName,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-all-players',
@@ -8,13 +14,32 @@ import { PlayerService } from '@app/services';
   styleUrls: ['./all-players.component.scss']
 })
 export class AllPlayersComponent implements OnInit {
+  deleteDialog: HTMLDialogElement;
+  deletePlayerForm = new FormGroup({
+    answer: new FormControl('no', Validators.pattern('yes'))
+  });
+  selectedPlayterToDelete: Player;
   players: Player[];
   constructor(private readonly playerService: PlayerService) {}
 
   ngOnInit() {
+    this.deleteDialog = document.querySelector('#delete-dialog');
+    this.playerService.fetchAllPlayers();
     this.playerService.players.subscribe(
       response => (this.players = response),
       error => console.error(error)
     );
+  }
+
+  openDeleteDialog(id) {
+    this.selectedPlayterToDelete = this.getPlayer(id);
+
+    this.deleteDialog.showModal();
+    console.log('id', id);
+    console.log('player', this.selectedPlayterToDelete);
+  }
+
+  getPlayer(id: string): Player {
+    return this.players.find(player => player._id === id);
   }
 }
