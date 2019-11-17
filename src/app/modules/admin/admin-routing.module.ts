a0e12bfb9d908f48041f00bab80b4ae3efe0b6f4
Daniel Weiß <wanieldeiss@gmail.com>
2019-11-17 17:37:18 +0100

    add add-score to routing

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AllPlayersComponent } from './pages/all-players/all-players.component';
 import { AddPlayersComponent } from './pages/add-players/add-players.component';
+import { AddScoreComponent } from './pages/add-score/add-score.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,8 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       { path: '', component: AllPlayersComponent },
-      { path: 'add-players', component: AddPlayersComponent }
+      { path: 'add-players', component: AddPlayersComponent },
+      { path: 'add-score', component: AddScoreComponent }
     ]
   }
 ];
