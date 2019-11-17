89c2f2e140765434599f87bdc69ab561316f2e28
Daniel Weiß <wanieldeiss@gmail.com>
2019-11-17 18:01:34 +0100

    select players to add the score

--- src/app/modules/admin/pages/add-score/add-score.component.ts
+++ src/app/modules/admin/pages/add-score/add-score.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '@app/services';
 
 @Component({
   selector: 'app-add-score',
   templateUrl: './add-score.component.html',
-  styleUrls: ['./add-score.component.sass']
+  styleUrls: ['./add-score.component.scss']
 })
 export class AddScoreComponent implements OnInit {
-
-  constructor() { }
+  existingPlayerNames: string[];
+  selectedPlayerNames: string[] = [];
+  constructor(private readonly playerService: PlayerService) {}
 
   ngOnInit() {
+    this.getPlayerNames();
   }
 
+  getPlayerNames(): void {
+    this.playerService
+      .getPlayerNames()
+      .subscribe((res: string[]) => (this.existingPlayerNames = res));
+  }
+
+  togglePlayer(player: string) {
+    if (this.selectedPlayerNames.includes(player)) {
+      const index = this.selectedPlayerNames.indexOf(player, 0);
+      if (index > -1) {
+        this.selectedPlayerNames.splice(index, 1);
+      }
+    } else {
+      this.selectedPlayerNames.push(player);
+    }
+  }
 }
